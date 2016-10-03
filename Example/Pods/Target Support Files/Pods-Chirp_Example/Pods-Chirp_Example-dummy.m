52f18ae10d16be9229696bf9ff721e242abb423e
Andrea Mazzini <andrea.mazzini@gmail.com>
2016-10-03 12:05:52 +0200

    Update example

--- Example/Pods/Target Support Files/Pods-Chirp_Example/Pods-Chirp_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Chirp_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Chirp_Example
-@end
--- Example/Pods/Target Support Files/Pods-Chirp_Example/Pods-Chirp_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Chirp_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Chirp_ExampleVersionString[];
-
